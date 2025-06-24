96fdb74c3443b8f4e04659e5780ac8652049bdfd
yn <jenny783113@gmail.com>
2025-06-24 16:34:17 +0800

    更新資料庫

--- db07.sql
+++ db07.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2025-06-24 03:27:43
+-- 產生時間： 2025-06-24 10:33:20
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -24,6 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `ad`
+--
+
+CREATE TABLE `ad` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `text` text DEFAULT NULL,
+  `sh` int(1) UNSIGNED DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `ad`
+--
+
+INSERT INTO `ad` (`id`, `text`, `sh`) VALUES
+(1, '轉知臺北教育大學與臺灣師大合辦第11屆麋研齋全國硬筆書法比賽活動', 1),
+(2, '轉知:法務部辦理「第五屆法規知識王網路闖關競賽辦法', 0),
+(3, '轉知2012年全國青年水墨創作大賽活動', 0),
+(5, '轉知:教育是人類升沉的樞紐-2013教師生命成長營', 1);
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `title`
 --
 
@@ -35,8 +57,24 @@ CREATE TABLE `title` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
+-- 傾印資料表的資料 `title`
+--
+
+INSERT INTO `title` (`id`, `text`, `img`, `sh`) VALUES
+(1, '卓越科技大學', '01B01.jpg', 0),
+(3, '卓越科技大學', '01B03.jpg', 0),
+(5, '卓越科技大學', '01B02.jpg', 0),
+(6, '卓越科技大學', '01B04.jpg', 1);
+
+--
 -- 已傾印資料表的索引
 --
+
+--
+-- 資料表索引 `ad`
+--
+ALTER TABLE `ad`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- 資料表索引 `title`
@@ -49,10 +87,16 @@ ALTER TABLE `title`
 --
 
 --
+-- 使用資料表自動遞增(AUTO_INCREMENT) `ad`
+--
+ALTER TABLE `ad`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- 使用資料表自動遞增(AUTO_INCREMENT) `title`
 --
 ALTER TABLE `title`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
