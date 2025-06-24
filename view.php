9a9b9a85da7771a7b125bb93f5a42b8137e7a845
yn <jenny783113@gmail.com>
2025-06-24 09:34:46 +0800

    建立標題管理需要的資料表

--- view.php
+++ view.php
@@ -1,7 +1,25 @@
 <?php
 switch($_GET['do']){
     case 'title':
-        echo "title";
+?>
+<h3 style="text-align: center;">新增標題區圖片</h3>
+<hr>
+<form action="">
+    <div>
+        <label>標題區圖片</label>
+        <input type="file" name="img" id="">
+    </div>
+    <div>
+        <label>標題區替代文字</label>
+        <input type="text" name="text" id="">
+    </div>
+    <div>
+        <input type="submit" value="新增">
+        <input type="reset" value="重置">
+    </div>
+</form>
+
+<?php
         break;
     case 'ad':
         echo "ad";
