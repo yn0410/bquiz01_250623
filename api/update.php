d5b83bab5e042bcd251e0aea1de2e1a258b71d5b
yn <jenny783113@gmail.com>
2025-06-27 13:33:41 +0800

    整併更新圖片api為一支

--- api/update.php
+++ api/update.php
@@ -1,13 +1,15 @@
 <?php include_once "db.php";
 
+$table=$_POST['table'];
+$db=${ucfirst($table)};
 
 if(!empty($_FILES['img']['tmp_name'])){
     move_uploaded_file($_FILES['img']['tmp_name'],"../images/".$_FILES['img']['name']);
-    $row=$Title->find($_POST['id']);
+    $row=$db->find($_POST['id']);
     $row['img']=$_FILES['img']['name'];
-    $Title->save($row);
+    $db->save($row);
 }
 
-to("../backend.php?do=title");
+to("../backend.php?do=$table");
 
 ?>
--- modal/update_mvim.php
+++ modal/update_mvim.php
@@ -1,12 +1,13 @@
 <h3 style="text-align: center;">更新動畫圖片</h3>
 <hr>
-<form action="./api/update_mvim.php" method='post' enctype="multipart/form-data">
+<form action="./api/update.php" method='post' enctype="multipart/form-data">
     <div>
-        <label>標題區圖片</label>
+        <label>動畫圖片</label>
         <input type="file" name="img">
     </div>
     <div>
         <input type="hidden" name="id" value="<?=$_GET['id'];?>">
+        <input type="hidden" name="table" value="<?=$_GET['table'];?>">
         <input type="submit" value="更新">
         <input type="reset" value="重置">
     </div>
