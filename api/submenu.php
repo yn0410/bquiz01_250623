4017548671abd0ef388fc6e903aab12aff8430fe
yn <jenny783113@gmail.com>
2025-06-30 15:44:41 +0800

    (老師的commit/完成次選單新增功能)

--- api/submenu.php
+++ api/submenu.php
@@ -1,8 +1,18 @@
 <?php include_once 'db.php';
+$main_id=$_POST['id'];
 
+if(isset($_POST['text2'])){
+    foreach($_POST['text2'] as $key => $text){
+        if($text!=""){
+            $href=$_POST['href2'][$key];
+            $Menu->save(['text'=>$text,
+                        'href'=>$href,
+                        'main_id'=>$main_id,
+                        'sh'=>1]);
+        }
+    }
+}
 
-
-
-
+to("../backend.php?do=menu");
 
 ?>
