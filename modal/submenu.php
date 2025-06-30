83cdd555664d7fcdebdfac8c41e613cbe10b8e08
yn <jenny783113@gmail.com>
2025-06-30 14:47:10 +0800

    (老師的commit/完成次選單版面設計及更多次選單功能)

--- modal/submenu.php
+++ modal/submenu.php
@@ -8,22 +8,38 @@
     </div>
     <div style="display:flex;margin:auto;width:70%" class="cent">
         <div style="width:45%;margin:5px 0.5%">
-            <input type="text" value="" style="width:95%">
+            <input type="text" name="text[]" value="" style="width:95%">
         </div>
         <div style="width:45%;margin:5px 0.5%">
-            <input type="text" value="" style="width:95%">
+            <input type="text" name="href[]" value="" style="width:95%">
         </div>
         <div style="width:10%;margin:5px 0.5%">
-            <input type="checkbox" value="" >
+            <input type="checkbox" name="del[]" value="" >
         </div>
     </div>
-
+    <div id="add"></div>
 
     <div class="cent">
         <input type="hidden" name="id" value="<?=$_GET['id'];?>">
         <input type="hidden" name="table" value="<?=$_GET['table'];?>">
         <input type="submit" value="修改確定">
         <input type="reset" value="重置">
-        <input type="button" value="更多次選單">
+        <input type="button" value="更多次選單" onclick="more()">
     </div>
 </form>
+
+<script>
+    function more(){
+        let item=`<div style="display:flex;margin:auto;width:70%" class="cent">
+        <div style="width:45%;margin:5px 0.5%">
+            <input type="text" name="text2[]" value="" style="width:95%">
+        </div>
+        <div style="width:45%;margin:5px 0.5%">
+            <input type="text" name="href2[]" value="" style="width:95%">
+        </div>
+        <div style="width:10%;margin:5px 0.5%">
+        </div>
+    </div>`;
+    $("#add").append(item);
+    }
+</script>
