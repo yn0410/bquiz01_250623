f5902a3878d140eabf181cc924a39d5e1557fd07
yn <jenny783113@gmail.com>
2025-07-01 09:01:26 +0800

    完成 前台&後台 網站標題區的圖片 的顯示

--- backend.php
+++ backend.php
@@ -18,8 +18,10 @@
 </div>
 
 	<div id="main">
-    	<a title="" href="?"><div class="ti" style="background:url(&#39;use/&#39;); background-size:cover;"></div><!--標題--></a>
-        	<div id="ms">
+		<a title="<?=$Title->find(['sh'=>1])['text'];?>" href="index.php">
+			<div class="ti" style="background:url(&#39;images/<?=$Title->find(['sh'=>1])['img'];?>&#39;); background-size:cover;"></div>
+			<!--標題-->
+		</a>        	<div id="ms">
              	<div id="lf" style="float:left;">
             		<div id="menuput" class="dbor">
                     <!--主選單放此-->
--- index.php
+++ index.php
@@ -17,7 +17,10 @@
     </div>
 </div>
 	<div id="main">
-    	<a title="" href="./home_files/home.htm"><div class="ti" style="background:url(&#39;use/&#39;); background-size:cover;"></div><!--標題--></a>
+    	<a title="<?=$Title->find(['sh'=>1])['text'];?>" href="index.php">
+			<div class="ti" style="background:url(&#39;images/<?=$Title->find(['sh'=>1])['img'];?>&#39;); background-size:cover;"></div>
+			<!--標題-->
+		</a>
         	<div id="ms">
              	<div id="lf" style="float:left;">
             		<div id="menuput" class="dbor">
